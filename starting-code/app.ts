1c9e0fb3c4943e6455b0cd073210fc46945fa492
Roberto Salim <salimroberto95@gmail.com>
2020-06-10 22:20:54 +0800

    end of 2.21. Function Types & Callbacks.mp4

--- starting-code/app.ts
+++ starting-code/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 // let combineValues: Function;
@@ -15,3 +20,7 @@ combineValues = add;
 // combineValues = printResult;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(9, 12, (res) => {
+  console.log(res);
+});
