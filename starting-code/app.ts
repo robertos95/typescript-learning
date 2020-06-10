87c269458c31f0cde061d4a2be61130ce37dfc10
Roberto Salim <salimroberto95@gmail.com>
2020-06-10 22:28:57 +0800

    end of 2.24. The never Type.mp4

--- starting-code/app.ts
+++ starting-code/app.ts
@@ -1,26 +1,15 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-// let combineValues: Function;
-let combineValues: (a: number, b: number) => number; // specify input params types and output type
-
-combineValues = add;
-// combineValues = printResult;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(9, 12, (res) => {
-  console.log(res);
-});
+generateError('An error occured!', 500);
