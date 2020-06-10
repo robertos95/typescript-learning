8c8fee11d92a99d7e3311d5ed67c3ad0e99bc5cc
Roberto Salim <salimroberto95@gmail.com>
2020-06-10 21:07:11 +0800

    end of 2.11. Working with Tuples.mp4

--- starting-code/app.ts
+++ starting-code/app.ts
@@ -1,10 +1,25 @@
-// const person: {
-//   name: String;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: String;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  // const person = {
   name: "Roberto",
   age: 25,
+  hobbies: ["Chess", "Toastmasters", "Mobile Legends"],
+  role: [2, "author"],
 };
 
+// person.role.push("admin"); // exception (tuple can't check push)
+// person.role[1] = 10;
+// person.role = [0, "admin", "user"];
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
