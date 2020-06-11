8e042b41afaee4a5192c38d4828ab6b40006da44
Roberto Salim <salimroberto95@gmail.com>
2020-06-12 05:57:15 +0800

    end of 8.6. Diving into Property Decorators.mp4

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -1,5 +1,5 @@
 function Logger(logString: string) {
-  console.log('LOGGER FACTORY');
+  console.log("LOGGER FACTORY");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -7,26 +7,55 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE FACTORY');
+  console.log("TEMPLATE FACTORY");
   return function (constructor: any) {
-    console.log('Rendering Template');
+    console.log("Rendering Template");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
       hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
   };
 }
 
-@Logger('LOGGING')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
+@Logger("LOGGING")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
-  name = 'Bob';
+  name = "Bob";
 
   constructor() {
-    console.log('Creating person object...');
+    console.log("Creating person object...");
   }
 }
 
 const pers = new Person();
+
+// ----------------------------------------------------------------
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
