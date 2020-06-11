2d84afbf220a396f2904962826402832f7e0f72b
Roberto Salim <salimroberto95@gmail.com>
2020-06-11 18:23:53 +0800

    end of 7.5. Another Generic Function.mp4

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,9 +1,25 @@
 // Code goes here!
 
-function merge<T extends object,U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
-const mergeObj = merge({name: 'Bob', hobbies: ['Sports']}, {age: 30});
+const mergeObj = merge({ name: "Bob", hobbies: ["Sports"] }, { age: 30 });
 
 console.log(mergeObj);
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value';
+    if(element.length > 0) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['one', 'two']));
+// countAndDescribe(12); // error
