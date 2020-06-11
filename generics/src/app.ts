34fb93eb3f18b59044d61c15cf461776fbd1f7a9
Roberto Salim <salimroberto95@gmail.com>
2020-06-11 18:37:18 +0800

    end of 7.7. Generic Classes.mp4

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -9,23 +9,56 @@ const mergeObj = merge({ name: "Bob", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergeObj);
 
 interface Lengthy {
-    length: number;
+  length: number;
 }
 
 function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-    let descriptionText = 'Got no value';
-    if(element.length > 0) {
-        descriptionText = 'Got ' + element.length + ' elements.';
-    }
-    return [element, descriptionText];
+  let descriptionText = "Got no value";
+  if (element.length > 0) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
 }
 
-console.log(countAndDescribe('Hi there!'));
-console.log(countAndDescribe(['one', 'two']));
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["one", "two"]));
 // countAndDescribe(12); // error
 
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-    return 'Value ' + obj[key];
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value " + obj[key];
 }
 
-extractAndConvert({name: 'Bob'}, 'name');
+extractAndConvert({ name: "Bob" }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Bob');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+// const objStorage = newDataStorage<object>();
+// objStorage.addItem({name: 'Max'});
+// objStorage.addItem({name: 'Bob'});
+// objStorage.removeItem({name: 'Max'}); // doesn't work because it's diff instance, must store in an att instead and refer to that att to work
