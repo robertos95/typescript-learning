7ca46a5971a3f178e4e78e3c19f8e3795410d61a
Roberto Salim <salimroberto95@gmail.com>
2020-06-13 03:58:30 +0800

    end of 13.4. Setting Up a Google API Key.mp4

--- prj-libs-01/src/app.ts
+++ prj-libs-01/src/app.ts
@@ -1,11 +1,14 @@
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address") as HTMLInputElement;
 
+const GOOGLE_API_KEY = CENSORED;
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-  
+
   //send this to Google's API!
+  
 }
 
 form.addEventListener("submit", searchAddressHandler);
