12ccc205074847453e80d329c960362e8c8728fb
Roberto Salim <salimroberto95@gmail.com>
2020-06-13 05:20:19 +0800

    end of 15.6. Working with Controllers & Parsing Request Bodies.mp4

--- node-01/src/routes/todos.ts
+++ node-01/src/routes/todos.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
+import { createTodo } from "../controllers/todos";
 
 const router = Router();
 
-router.post('/');
+router.post("/", createTodo);
 
-router.get('/');
+router.get("/");
 
-router.patch('/:id');
+router.patch("/:id");
 
-router.delete('/:id');
-
+router.delete("/:id");
 
 export default router;
