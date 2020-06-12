2afe34833a23d2ab4530f048259252f4048ff30e
Roberto Salim <salimroberto95@gmail.com>
2020-06-13 03:27:46 +0800

    end of 12.5. TypeScript-embracing class-validator.mp4

--- libs/src/app.ts
+++ libs/src/app.ts
@@ -1,5 +1,6 @@
 import { Product } from "./product.model";
 import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 
 // const p1 = new Product("A book", 12.99);
 // console.log(p1.getInformation());
@@ -18,3 +19,13 @@ const loadedProducts = plainToClass(Product, products);
 for (const p of loadedProducts) {
   console.log(p.getInformation());
 }
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
