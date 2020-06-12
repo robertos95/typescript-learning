9ccb9c87c06c7312c2e84236e788d971e97c8f65
Roberto Salim <salimroberto95@gmail.com>
2020-06-12 15:32:03 +0800

    end of 9.4. Interacting with DOM Elements.mp4

--- prj-00/src/app.ts
+++ prj-00/src/app.ts
@@ -4,6 +4,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
@@ -11,8 +14,24 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = <HTMLFormElement>importedNode.firstElementChild!;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
+        this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
+        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people');
+
+        this.configure();
         this.attach();
 
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
